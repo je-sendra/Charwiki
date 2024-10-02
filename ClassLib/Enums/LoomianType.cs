485e1662037651a057d5fd7d3de5fdb699537fbe
Josep <je.sendra@outlook.es>
2024-10-02 21:02:29 +0200

    feat: Added Loomian types to the LoomianType enum

--- ClassLib/Enums/LoomianType.cs
+++ ClassLib/Enums/LoomianType.cs
@@ -5,5 +5,88 @@ namespace Charwiki.ClassLib.Enums;
 /// </summary>
 public enum LoomianType
 {
-    
+    /// <summary>
+    /// Represents the Fire type.
+    /// </summary>
+    Fire,
+
+    /// <summary>
+    /// Represents the Water type.
+    /// </summary>
+    Water,
+
+    /// <summary>
+    /// Represents the Grass type.
+    /// </summary>
+    Plant,
+
+    /// <summary>
+    /// Represents the Light type.
+    /// </summary>
+    Light,
+
+    /// <summary>
+    /// Represents the Dark type.
+    /// </summary>
+    Dark,
+
+    /// <summary>
+    /// Represents the Ice type.
+    /// </summary>
+    Ice,
+
+    /// <summary>
+    /// Represents the Electric type.
+    /// </summary>
+    Electric,
+
+    /// <summary>
+    /// Represents the Air type.
+    /// </summary>
+    Air,
+
+    /// <summary>
+    /// Represents the Bug type.
+    /// </summary>
+    Bug,
+
+    /// <summary>
+    /// Represents the Earth type.
+    /// </summary>
+    Earth,
+
+    /// <summary>
+    /// Represents the Toxic type.
+    /// </summary>
+    Toxic,
+
+    /// <summary>
+    /// Represents the Metal type.
+    /// </summary>
+    Metal,
+
+    /// <summary>
+    /// Represents the Ancient type.
+    /// </summary>
+    Ancient,
+
+    /// <summary>
+    /// Represents the Spirit type.
+    /// </summary>
+    Spirit,
+
+    /// <summary>
+    /// Represents the Brawler type.
+    /// </summary>
+    Brawler,
+
+    /// <summary>
+    /// Represents the Mind type.
+    /// </summary>
+    Mind,
+
+    /// <summary>
+    /// Represents the Simple type.
+    /// </summary>
+    Simple
 }
