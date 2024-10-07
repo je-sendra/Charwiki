bd618d21f2ae7b5e434a0bd26686180ae0491072
Josep Sendra <je.sendra@lokishell.com>
2024-10-07 21:53:38 +0200

    feat: Add ApiSettings class for API configuration

---
+++ ClassLib/Configuration/ApiSettings.cs
@@ -0,0 +1,12 @@
+namespace Charwiki.ClassLib.Configuration;
+
+/// <summary>
+/// Represents the settings for the API.
+/// </summary>
+public class ApiSettings
+{
+    /// <summary>
+    /// Gets or sets the base URL of the API.
+    /// </summary>
+    public required string BaseUrl { get; set; }
+}
