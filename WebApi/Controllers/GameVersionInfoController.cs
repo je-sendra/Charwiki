b73476f4fbdb7060bab4eb5df303b28de6dde75b
Josep Sendra <je.sendra@lokishell.com>
2024-10-08 09:31:20 +0200

    Make API controllers generic Fixes #2

--- WebApi/Controllers/GameVersionInfoController.cs
+++ WebApi/Controllers/GameVersionInfoController.cs
@@ -1,6 +1,9 @@
 using Charwiki.ClassLib.Models;
+using Charwiki.WebApi.Controllers.Templates;
+using Charwiki.WebApi.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Charwiki.WebApi.Controllers;
 
@@ -9,57 +12,15 @@ namespace Charwiki.WebApi.Controllers;
 /// </summary>
 /// <param name="charwikiDbContext"></param>
 [Route("[controller]")]
-public class GameVersionInfosController(CharwikiDbContext charwikiDbContext) : ControllerBase
+public class GameVersionInfosController(CharwikiDbContext charwikiDbContext) : CrudControllerTemplate<GameVersionInfo>(charwikiDbContext, charwikiDbContext.GameVersionInfos)
 {
-    /// <summary>
-    /// Endpoint to get all game version infos.
-    /// </summary>
-    /// <returns></returns>
-    [HttpGet]
-    public IActionResult GetAllGameVersionInfos()
-    {
-        return Ok(charwikiDbContext.GameVersionInfos);
-    }
-
-    /// <summary>
-    /// Endpoint to get a specific game version info.
-    /// </summary>
-    /// <param name="id"></param>
-    /// <returns></returns>
-    [HttpGet("{id}")]
-    public IActionResult GetById(Guid id)
-    {
-        if (!ModelState.IsValid)
-        {
-            return BadRequest(ModelState);
-        }
-
-        var gameVersionInfo = charwikiDbContext.GameVersionInfos.FirstOrDefault(e => e.Id == id);
-        if (gameVersionInfo == null)
-        {
-            return NotFound();
-        }
-
-        return Ok(gameVersionInfo);
-    }
-
-    /// <summary>
-    /// Endpoint to create a new game version info.
-    /// </summary>
-    /// <param name="gameVersionInfo"></param>
-    /// <returns></returns>
+    /// <inheritdoc />
     [HttpPost]
-    [Authorize(Roles="Admin")]
-    public IActionResult CreateGameVersionInfo([FromBody] GameVersionInfo gameVersionInfo)
+    [Authorize(Roles = "Admin")]
+    #pragma warning disable S6967
+    public override async Task<IActionResult> CreateNew([FromBody] GameVersionInfo gameVersionInfo)
     {
-        if (!ModelState.IsValid)
-        {
-            return BadRequest(ModelState);
-        }
-
-        charwikiDbContext.GameVersionInfos.Add(gameVersionInfo);
-        charwikiDbContext.SaveChanges();
-
-        return Created($"/gameversioninfos/{gameVersionInfo.Id}", gameVersionInfo);
+        return await base.CreateNew(gameVersionInfo);
     }
+    #pragma warning restore S6967
 }
