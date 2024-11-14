9a864d22e5a7584e3e197f05df53819956b11940
je-sendra <josep@sendraserra.com>
2024-11-14 23:07:25 +0100

    feat(classlib): Implement LoomianSetsService and mock service for Loomian set operations

---
+++ ClassLib/Services/ILoomianSetsService.cs
@@ -0,0 +1,12 @@
+using Charwiki.ClassLib.Models;
+using Charwiki.ClassLib.Services.Templates;
+
+namespace Charwiki.ClassLib.Services;
+
+/// <summary>
+/// Represents a service for Loomian set-related operations.
+/// </summary>
+public interface ILoomianSetsService : ICrudControllerServiceTemplate<LoomianSet>
+{
+    
+}
