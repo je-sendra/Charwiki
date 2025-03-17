4442869393c4a2d0f0ef96631e45f0bac2cda688
je-sendra <je.sendra@outlook.es>
2025-03-17 16:06:11 +0100

    feat(webapi): add endpoint to retrieve current user without sensitive information

---
+++ ClassLib/Extensions/UserExtensions.cs
@@ -0,0 +1,25 @@
+using Charwiki.ClassLib.Exceptions;
+using Charwiki.ClassLib.Models;
+
+namespace Charwiki.ClassLib.Extensions;
+
+/// <summary>
+/// Contains extension methods for <see cref="User"/>.
+/// </summary>
+public static class UserExtensions
+{
+    /// <summary>
+    /// Returns the user with all sensitive information removed.
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns></returns>
+    public static User GetCopyWithoutSensitiveInformation(this User user)
+    {
+        return new User
+        {
+            Id = user.Id,
+            Username = user.Username,
+            Role = user.Role
+        };
+    }
+}
