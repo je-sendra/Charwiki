753015403175c3d590b8d52b682bb4a6f83a1af0
je-sendra <josep@sendraserra.com>
2025-07-02 23:12:31 +0200

    feat: add filtering to Home page

--- WebUi/Components/Pages/Home.razor.cs
+++ WebUi/Components/Pages/Home.razor.cs
@@ -18,24 +18,57 @@ public partial class Home
 
     private IEnumerable<LoomianSetResponseDto> LoomianSets { get; set; } = [];
 
+    private Guid? SelectedLoomianId { get; set; }
+    private Guid? SelectedAbilityId { get; set; }
+    private Guid? SelectedItemId { get; set; }
+    private Guid? SelectedMoveId { get; set; }
+    private Guid? SelectedGameVersionInfoId { get; set; }
+    private Guid? SelectedTagId { get; set; }
+
     /// <inheritdoc/>
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
         if (firstRender)
         {
-            LoomianSetQueryParams queryParams = new()
-            {
-                HideNonApprovedSets = false,
-                IncludeMetadata = true,
-                IncludeLoomian = true,
-                IncludeAverageRating = true,
-            };
-            IEnumerable<LoomianSetResponseDto>? loomianSetsFromDb = await LoomianSetsService.GetAllAsync(queryParams);
-            if (loomianSetsFromDb != null)
-            {
-                LoomianSets = loomianSetsFromDb;
-            }
+            SelectedLoomianId = null;
+            SelectedAbilityId = null;
+            SelectedItemId = null;
+            SelectedMoveId = null;
+            SelectedGameVersionInfoId = null;
+            SelectedTagId = null;
+
+            await UpdateLoomianSetsAsync();
+
             StateHasChanged();
         }
+    }
+
+    private async Task UpdateLoomianSetsAsync()
+    {
+        LoomianSetQueryParams queryParams = new()
+        {
+            HideNonApprovedSets = false,
+            IncludeMetadata = true,
+            IncludeLoomian = true,
+            IncludeAverageRating = true,
+            IncludeTags = true,
+            LoomianId = SelectedLoomianId,
+            AbilityId = SelectedAbilityId,
+            ItemId = SelectedItemId,
+            MoveId = SelectedMoveId,
+            GameVersionInfoId = SelectedGameVersionInfoId,
+        };
+        if (SelectedTagId != null)
+        {
+            queryParams.TagsIds = [SelectedTagId.Value];
+        }
+
+        IEnumerable<LoomianSetResponseDto>? loomianSetsFromDb = await LoomianSetsService.GetAllAsync(queryParams);
+        if (loomianSetsFromDb != null)
+        {
+            LoomianSets = loomianSetsFromDb;
+        }
+
+        StateHasChanged();
     }
 }
