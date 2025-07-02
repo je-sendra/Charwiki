3a111d356aef71026eededed194acb28b6697b7b
je-sendra <josep@sendraserra.com>
2025-07-02 22:17:06 +0200

    refactor: use DTOs on ClassLib services

--- ClassLib/Services/GameVersionInfosService.cs
+++ ClassLib/Services/GameVersionInfosService.cs
@@ -1,4 +1,6 @@
+using System.Net.Http.Json;
 using Charwiki.ClassLib.Configuration;
+using Charwiki.ClassLib.Dto.Response;
 using Charwiki.ClassLib.Models;
 using Charwiki.ClassLib.Services.Templates;
 using Microsoft.Extensions.Options;
@@ -10,6 +12,17 @@ namespace Charwiki.ClassLib.Services;
 /// </summary>
 /// <param name="httpClient"></param>
 /// <param name="apiSettings"></param>
-public class GameVersionInfosService(HttpClient httpClient, IOptions<ApiSettings> apiSettings) : CrudControllerServiceTemplate<GameVersionInfo>(httpClient, apiSettings, "gameVersionInfos"), IGameVersionInfosService
+public class GameVersionInfosService(HttpClient httpClient, IOptions<ApiSettings> apiSettings) : IGameVersionInfosService
 {
+    /// <inheritdoc/>
+    public async Task<IEnumerable<GameVersionInfoResponseDto>> GetAllAsync()
+    {
+        HttpResponseMessage response = await httpClient.GetAsync($"{apiSettings.Value.BaseUrl}/gameVersionInfos");
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new HttpRequestException(await response.Content.ReadAsStringAsync() ?? response.ReasonPhrase);
+        }
+        IEnumerable<GameVersionInfoResponseDto>? gameVersionInfos = await response.Content.ReadFromJsonAsync<IEnumerable<GameVersionInfoResponseDto>>();
+        return gameVersionInfos ?? [];
+    }
 }
--- ClassLib/Services/IGameVersionInfosService.cs
+++ ClassLib/Services/IGameVersionInfosService.cs
@@ -1,12 +1,15 @@
-using Charwiki.ClassLib.Models;
-using Charwiki.ClassLib.Services.Templates;
+using Charwiki.ClassLib.Dto.Response;
 
 namespace Charwiki.ClassLib.Services;
 
 /// <summary>
 /// Represents a service for game version info-related operations.
 /// </summary>
-public interface IGameVersionInfosService : ICrudControllerServiceTemplate<GameVersionInfo>
+public interface IGameVersionInfosService
 {
-    
+    /// <summary>
+    /// Gets all game version infos.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation, containing a collection of game version infos.</returns>
+    Task<IEnumerable<GameVersionInfoResponseDto>> GetAllAsync();
 }
